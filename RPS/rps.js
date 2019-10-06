005e8b848892a3e90c443ea06d166d305f31b63e
James Edens <jedensuscg@gmail.com>
2019-10-06 02:28:25 -0400

    Changed rules to first to 5

--- RPS/rps.js
+++ RPS/rps.js
@@ -33,13 +33,14 @@ playGame();
 
 // Functions
 function playGame() {
-    getPlayerSelection();
+    playRound();
     restart.addEventListener('click', (e) => {
         reload();
     });
+    
 }
 
-function playRound(playerselection, computerSelection) {
+function updateScores(playerselection, computerSelection) {
 
     console.log(`You Chose: ${playerselection}`)
     console.log(`Computers Chose: ${computerText(computerSelection)}`)
@@ -47,7 +48,7 @@ function playRound(playerselection, computerSelection) {
     console.log(`Round: ${round}`)
 
 
-    let win;
+    let roundWinner;
 
     var roundStatus;
     switch (playerselection) {
@@ -55,19 +56,19 @@ function playRound(playerselection, computerSelection) {
             if (computerSelection == 1) {
                 console.log("You Tied");
                 roundStatus = "You Tied";
-                win = winner.TIE;
+                roundWinner = winner.TIE;
                 ++tieScore;
             }
             else if (computerSelection == 2) {
                 console.log(`${paperRock} YOU LOSE`);
                 roundStatus = "You LOST";
-                win = winner.COMPUTER;
+                roundWinner = winner.COMPUTER;
                 ++compScore;
             }
             else {
                 console.log(`${rockScissor} YOU WIN`);
                 roundStatus = "You WON";
-                win = winner.PLAYER;
+                roundWinner = winner.PLAYER;
                 ++playerScore;
             }
             break;
@@ -75,19 +76,19 @@ function playRound(playerselection, computerSelection) {
             if (computerSelection == 1) {
                 console.log(`${paperRock} YOU WIN!`);
                 roundStatus = "You WON";
-                win = winner.PLAYER;
+                roundWinner = winner.PLAYER;
                 ++playerScore;
             }
             else if (computerSelection == 2) {
                 console.log(`YOU TIED`);
                 roundStatus = "You Tied";
-                win = winner.TIE;
+                roundWinner = winner.TIE;
                 ++tieScore
             }
             else {
                 console.log(`${paperScissor} YOU LOSE!`);
                 roundStatus = "You LOST";
-                win = winner.COMPUTER;
+                roundWinner = winner.COMPUTER;
                 ++compScore;
             }
             break;
@@ -95,148 +96,186 @@ function playRound(playerselection, computerSelection) {
             if (computerSelection == 1) {
                 console.log(`${rockScissor} YOU LOSE!`)
                 roundStatus = "You LOST";
-                win = winner.COMPUTER;
+                roundWinner = winner.COMPUTER;
                 ++compScore;
             }
             else if (computerSelection == 2) {
                 console.log(`${paperScissor} YOU WIN`)
                 roundStatus = "You WON";
-                win = winner.PLAYER;
+                roundWinner = winner.PLAYER;
                 ++playerScore;
             }
             else {
                 console.log(`YOU TIED`)
                 roundStatus = "You Tied";
-                win = winner.TIE;
+                roundWinner = winner.TIE;
                 ++tieScore
             }
             break;
     }
     roundLog.textContent = `You Chose: ${playerselection} : Computer Chose: ${computerText(computerSelection)}` + "\n" + `${roundStatus} this round.`;
-    displayScore(win);
+    updateScoreboard(roundWinner);
     displayImage(playerselection, computerSelection);
+    checkForWinner();
+
 }
 
-function getPlayerSelection() {
+function checkForWinner() {
+    if(compScore == 5 || playerScore == 5) {
+        endGame();
+    }
+}
 
-    //if (round < numberOfRounds) {
+/* function playRound() {
+    let scoreboardClear = 0;
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
-            if (round <= numberOfRounds - 1) {
-                playRound(e.target.value, getComputerSelection());
-                if (round == numberOfRounds) {
-                    endGame();
-                }
+            if (scoreboardClear <= 5) {
+                console.log("scoreboardClear:" + scoreboardClear)
+                updateScores(e.targetvalue, getComputerSelection());
+                scoreboardClear++;
             }
+            if (scoreboardClear > 5) {
+                scoreboardClear = 0;
+                clearScoreboard();
+                updateScores(e.target.value, getComputerSelection());
+                scoreboardClear++;
+            } */
 
+function playRound() {
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            if(round == 0){
+                updateScores(e.target.value, getComputerSelection());
+            }
+            else if ((round % 5) != 0) {
+                updateScores(e.target.value, getComputerSelection());
+            }
+            else if(round % 5 == 0){
+                clearScoreboard();
+                updateScores(e.target.value, getComputerSelection());
+            }       
         });
     });
-    // }
+
+
 }
 function getComputerSelection() {
     let compInput = randomInt(1, 3);
     return compInput;
 }
 
-function displayScore(win) {
-    console.log(win);
-    console.log(winner.PLAYER);
+function updateScoreboard(roundWinner) {
 
     playerTotal = document.querySelector('#scorePlayer');
     compTotal = document.querySelector('#scoreComp');
-    let roundNumber = 1;
     let playerWin;
     let computerWin;
-    let roundSpan = document.createElement("span");
-    let playerSpan = document.createElement("span");
-    let compSpan = document.createElement("span");
-    roundSpan.textContent = round;
-    spanRound = roundColumn.appendChild(roundSpan);
-    spanRound.classList.add("newRound");
-    if (win == winner.PLAYER) {
+    let scoreboardRoundNumber = document.createElement("span");
+    let scoreboardPlayerSpan = document.createElement("span");
+    let scoreboardCompSpan = document.createElement("span");
+    scoreboardRoundNumber.textContent = round;
+    spanRound = roundColumn.appendChild(scoreboardRoundNumber);
+    spanRound.classList.add("roundNumberScorboard");
+    
+    if (roundWinner == winner.PLAYER) {
         playerWin = "W";
         computerWin = "L";
-        playerSpan.style.color = colorWin;
-        compSpan.style.color = colorLose;
+        scoreboardPlayerSpan.style.color = colorWin;
+        scoreboardCompSpan.style.color = colorLose;
         document.getElementById("playerTitle").style.backgroundColor = colorWin;
         document.getElementById("compTitle").style.backgroundColor = colorLose;
     }
-    if (win == winner.COMPUTER) {
+    if (roundWinner == winner.COMPUTER) {
         computerWin = "W";
         playerWin = "L";
-        playerSpan.style.color = colorLose;
-        compSpan.style.color = colorWin;
+        scoreboardPlayerSpan.style.color = colorLose;
+        scoreboardCompSpan.style.color = colorWin;
         document.getElementById("playerTitle").style.backgroundColor = colorLose;
         document.getElementById("compTitle").style.backgroundColor = colorWin;
     }
-    if (win == winner.TIE) {
+    if (roundWinner == winner.TIE) {
         playerWin = "T";
         computerWin = "T";
-        playerSpan.style.color = colorTie;
-        compSpan.style.color = colorTie;
+        scoreboardPlayerSpan.style.color = colorTie;
+        scoreboardCompSpan.style.color = colorTie;
         document.getElementById("playerTitle").style.backgroundColor = colorTie;
         document.getElementById("compTitle").style.backgroundColor = colorTie;
         document.getElementById("compTitle").style.color = "black";
         document.getElementById("playerTitle").style.color = "black";
     }
 
-    playerSpan.textContent = playerWin;
-    compSpan.textContent = computerWin;
+    scoreboardPlayerSpan.textContent = playerWin;
+    scoreboardCompSpan.textContent = computerWin;
     console.log(playerWin);
-    spanP = playerColumn.appendChild(playerSpan);
-    spanP.classList.add("newPlayer");
-    spanC = compColumn.appendChild(compSpan);
-    spanC.classList.add("newComp");
+    spanP = playerColumn.appendChild(scoreboardPlayerSpan);
+    spanP.classList.add("playerRoundStatus");
+    spanC = compColumn.appendChild(scoreboardCompSpan);
+    spanC.classList.add("compRoundStatus");
     playerTotal.textContent = playerScore;
     compTotal.textContent = compScore;
 
 
 }
 
-function displayImage(playerselection, computerSelection){
+function displayImage(playerselection, computerSelection) {
     console.log("selection:" + computerSelection)
-    if(playerselection == 'PAPER') {
+    if (playerselection == 'PAPER') {
         document.getElementById("playerSRC").src = image.PAPER;
     }
-    if(playerselection == 'ROCK') {
+    if (playerselection == 'ROCK') {
         document.getElementById("playerSRC").src = image.ROCK;
     }
-    if(playerselection == 'SCISSORS') {
+    if (playerselection == 'SCISSORS') {
         document.getElementById("playerSRC").src = image.SCISSORS;
     }
 
-    if(computerSelection == 2) {
+    if (computerSelection == 2) {
         document.getElementById("compSRC").src = image.PAPER;
     }
-    if(computerSelection == 1) {
+    if (computerSelection == 1) {
         document.getElementById("compSRC").src = image.ROCK;
     }
-    if(computerSelection == 3) {
+    if (computerSelection == 3) {
         document.getElementById("compSRC").src = image.SCISSORS;
     }
 
+}
+
+function clearScoreboard(){
+    PlayerRoundStatusChild = document.querySelectorAll(".playerRoundStatus");
+    compRoundStatusChild = document.querySelectorAll(".compRoundStatus");
+    playerScoreboard = document.getElementById("playerScore");
+    compScoreboard = document.getElementById("compScore");
+    scoreboardRoundNumber = document.querySelectorAll(".roundNumberScorboard");
+    parentRound = document.getElementById("roundNum");
+    PlayerRoundStatusChild.forEach(child => playerScoreboard.removeChild(child));
+    compRoundStatusChild.forEach(child => compScoreboard.removeChild(child));
+    scoreboardRoundNumber.forEach(child => parentRound.removeChild(child));
 }
 
 function finalScore() {
     let endgameMessage = `Player Won ${playerScore} rounds, Computer won ${compScore} rounds, ${tieScore} rounds were a TIE.`
     let endgameWinner;
     let winFlag = document.getElementById("winnerFlag");
-    if (playerScore > compScore) {
+    if (playerScore == 5) {
         endgameWinner = "You WON the Game";
         endgameColor = colorWin;
         winFlag.classList.remove("inProgress");
         winFlag.classList.add("playerWin");
     }
-    else if (playerScore == compScore) {
-        endgameWinner = "You Tied the Game";
-        endgameColor = colorTie;
-    }
-    else {
+    if(compScore == 5){
         endgameWinner = "You LOST the Game"
         endgameColor = colorLose;
         winFlag.classList.remove("inProgress");
         winFlag.classList.add("compWin");
     }
+/*     else if (playerScore == compScore) {
+        endgameWinner = "You Tied the Game";
+        endgameColor = colorTie;
+    }
+ */
     roundLog.textContent = `${endgameMessage} \n ${endgameWinner}`
     roundLog.style.color = endgameColor;
 
@@ -263,7 +302,6 @@ function computerText(computerChoice) // Converts computer selection from intege
 }
 
 function endGame() {
-    console.log("done");
 
     buttons.forEach((button) => {
         button.removeEventListener('click', (e) => {
@@ -272,6 +310,8 @@ function endGame() {
     });
     finalScore();
 }
+
+
 
 function reload() {
     console.log("Restarting Game")
@@ -285,25 +325,16 @@ function reload() {
     document.getElementById("compTitle").style.backgroundColor = "";
     document.getElementById("compTitle").style.color = "";
     document.getElementById("playerTitle").style.color = "";
-    if(document.getElementById("winnerFlag").classList.contains("playerWin")){
+    if (document.getElementById("winnerFlag").classList.contains("playerWin")) {
         document.getElementById("winnerFlag").classList.remove("playerWin");
     }
-    if(document.getElementById("winnerFlag").classList.contains("compWin")){
+    if (document.getElementById("winnerFlag").classList.contains("compWin")) {
         document.getElementById("winnerFlag").classList.remove("compWin");
     }
     document.getElementById("winnerFlag").classList.add("inProgress");
 
-
-
-    childrenP = document.querySelectorAll(".newPlayer");
-    childrenC = document.querySelectorAll(".newComp");
-    parentP = document.getElementById("playerScore");
-    parentC = document.getElementById("compScore");
-    childrenRound = document.querySelectorAll(".newRound");
-    parentRound = document.getElementById("roundNum");
-    childrenP.forEach(child => parentP.removeChild(child));
-    childrenC.forEach(child => parentC.removeChild(child));
-    childrenRound.forEach(child => parentRound.removeChild(child));
+    clearScoreboard();
+    
     document.getElementById("playerSRC").src = "images/transparent.png";
     document.getElementById("compSRC").src = "images/transparent.png";
 }
